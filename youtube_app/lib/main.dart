4bf130954e6b0127dfe88dc42f203c00ca5a842f
juhyunham <judev0227@gmail.com>
2022-03-05 16:01:16 +0900

    초기셋팅

---
+++ youtube_app/lib/main.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:youtube_clone_app/src/app.dart';
+
+void main() {
+  runApp(const myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: "너튜브 App",
+      theme: ThemeData(
+        primaryColor: Colors.white, 
+        primarySwatch: Colors.blue, 
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: "/",
+      getPages: [
+        GetPage(name: "/", page: () => App())
+      ]
+    );
+  }
+}
